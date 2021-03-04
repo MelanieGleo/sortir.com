ff68a6c777eb647e739b84a536468118b7b75890
Marine <marinebdn4@gmail.com>
2021-03-04 16:43:51 +0100

    Page co Invalid Credentials + se souvenir de moi

--- src/Entity/Participant.php
+++ src/Entity/Participant.php
@@ -6,11 +6,12 @@ use App\Repository\ParticipantRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=ParticipantRepository::class)
  */
-class Participant
+class Participant implements UserInterface
 {
     /**
      * @ORM\Id
@@ -50,7 +51,7 @@ class Participant
     private $actif;
 
     /**
-     * @ORM\ManyToMany(targetEntity=sortie::class, inversedBy="participants")
+     * @ORM\ManyToMany(targetEntity=Sortie::class, inversedBy="participants")
      */
     private $sorties;
 
@@ -70,7 +71,7 @@ class Participant
     private $pseudo;
 
     /**
-     * @ORM\Column(type="text")
+     * @ORM\Column(type="string", length=64)
      */
     private $motDePasse;
 
@@ -246,4 +247,31 @@ class Participant
 
         return $this;
     }
+
+    public function getRoles()
+    {
+        // TODO: Implement getRoles() method.
+    }
+
+    public function getPassword()
+    {
+        // TODO: Implement getPassword() method.
+    }
+
+    public function getSalt()
+    {
+        // TODO: Implement getSalt() method.
+    }
+
+    public function getUsername()
+    {
+        // TODO: Implement getUsername() method.
+    }
+
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+
+
 }
