d68827fc0905120060faa0db4e212b84ec48c2e0
JB <jean-baptiste.ponnet9@orange.fr>
2021-03-11 16:23:21 +0100

    ajout filtres sites

--- src/Controller/Sortie/SortieController.php
+++ src/Controller/Sortie/SortieController.php
@@ -4,8 +4,11 @@ namespace App\Controller\Sortie;
 
 use App\Entity\Participant;
 use App\Entity\Sortie;
+use App\Repository\SiteRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,11 +18,23 @@ class SortieController extends AbstractController
      * @Route("/", name="app_sortie")
      **/
 
-    public function sortie(): Response
+    public function sortie(SiteRepository $siteRepo, Request $request): Response
     {
         $repo = $this->getDoctrine()->getRepository(Sortie::class);
-        $sorties = $repo->InfosSorties();
-        return $this->render('sortie/sortie.html.twig', ['sorties' => $sorties]);
+        $sites = $siteRepo->findAll();
+
+        //filtres
+        $filters = $request->get("sites");
+
+        $sorties = $repo->InfosSorties($filters);
+
+        if($request->get('ajax')){
+            return new JsonResponse([
+                'content' => $this->renderView('sortie/_contentSortie.html.twig', ['sorties' => $sorties])
+            ]);
+        }
+
+        return $this->render('sortie/sortie.html.twig', ['sorties' => $sorties, 'sites' => $sites]);
     }
 
     /**
